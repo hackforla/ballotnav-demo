9500a486f11f7c3ffbca33431d691d5e1adba67c
Jake Mensch <jake.mensch@gmail.com>
2021-03-10 17:08:42 -0800

    Updates

---
+++ static/js/main.c0248ba8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[2],[function(e,n,s){Promise.all([s.e(0),s.e(4),s.e(1),s.e(5)]).then(s.bind(null,2))}],[[0,3]]]);
+//# sourceMappingURL=main.c0248ba8.chunk.js.map
